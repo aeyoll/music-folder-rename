1d174269cada6ea0065c791b90659cb6b8d4ccca
Jean-Philippe Bidegain <jp@bidega.in>
2015-08-15 22:42:36 +0200

    Switched tag library

--- main.go
+++ main.go
@@ -2,22 +2,78 @@ package main
 
 import (
   "fmt"
+  "os"
   "log"
-  id3 "github.com/mikkyang/id3-go"
+  "strconv"
+  "path/filepath"
+  tag "github.com/dhowden/tag"
 )
 
-func main () {
-  // Opening the file
-  mp3File, err := id3.Open("test.mp3")
+func getNewFileName(m tag.Metadata) string {
+  // Artist
+  artist := m.AlbumArtist()
 
-  if err != nil {
-    log.Fatal(err)
-  } else {
-    fmt.Println(mp3File.Artist())
-    fmt.Println(mp3File.Year())
-    fmt.Println(mp3File.Album())
+  if artist == "" {
+    artist = m.Artist()
   }
 
-  // Closing the file
-  defer mp3File.Close()
+  // Year
+  year := strconv.Itoa(m.Year())
+
+  // Album
+  album := m.Album()
+
+  // New folder
+  folder := artist + " - " + year + " - " + album
+
+  return folder
+}
+
+func main () {
+  // Get the folders passed as arguments
+  folders := os.Args[1:]
+
+  for _,folder := range folders {
+    d, err := os.Open(folder)
+
+    if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+    }
+
+    defer d.Close()
+
+    files, err := d.Readdir(-1)
+    if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+    }
+
+    for _, file := range files {
+      if file.Mode().IsRegular() {
+        if filepath.Ext(file.Name()) == ".mp3" {
+          var f, err = os.Open(folder + "/" + file.Name())
+
+          if err != nil {
+            log.Fatal(err)
+          }
+
+          defer f.Close()
+
+          m, err := tag.ReadFrom(f)
+
+          if err != nil {
+            log.Fatal(err)
+          }
+
+          var newFolderName = getNewFileName(m)
+          fmt.Println(newFolderName);
+
+          os.Rename(folder, newFolderName)
+
+          break
+        }
+      }
+    }
+  }
 }
